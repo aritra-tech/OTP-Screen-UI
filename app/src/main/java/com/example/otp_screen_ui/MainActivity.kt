0d3b31214030034635151693bcd7473638d09574
Aritra Das <aritrarick2002@gmail.com>
2023-01-17 02:59:37 +0530

    Intial commit 🐛

---
+++ app/src/main/java/com/example/otp_screen_ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.otp_screen_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setContentView(R.layout.activity_main)
+
+//        supportActionBar?.hide()
+    }
+}
